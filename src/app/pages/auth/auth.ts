24143aee4313c9b19a983af73666ff7d1f817875
Emmanuel Kpendo <devekkx@gmail.com>
2025-08-08 10:48:30 +0000

    feature: update auth flow

--- src/app/pages/auth/auth.ts
+++ src/app/pages/auth/auth.ts
@@ -5,7 +5,7 @@ import {InputText} from 'primeng/inputtext';
 import {FormControl, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
 import {cn, ghPhoneValidator, matchPasswordValidator, strongPasswordValidator} from '@/lib/utils';
 import {NgTemplateOutlet, TitleCasePipe} from '@angular/common';
-import {ButtonDirective} from 'primeng/button';
+import {Button, ButtonDirective} from 'primeng/button';
 import {Password} from 'primeng/password';
 import {InputMask} from 'primeng/inputmask';
 
@@ -20,7 +20,8 @@ import {InputMask} from 'primeng/inputmask';
     ButtonDirective,
     Password,
     InputMask,
-    NgTemplateOutlet
+    NgTemplateOutlet,
+    Button
   ],
   templateUrl: './auth.html',
   styleUrl: './auth.css'
@@ -28,16 +29,16 @@ import {InputMask} from 'primeng/inputmask';
 export class Auth {
   protected authForm: FormGroup;
   protected authFormControls = signal<string[]>([]);
-  protected formType = signal<"login" | "signup">("signup")
+  protected formType = signal<"login" | "signup">("login")
   protected minLengthValidator = Validators.minLength(5);
   protected readonly cn = cn;
 
 
   constructor() {
-    this.authForm = this.formType() === "login" ? this.createLoginForm() : this.createSignUpForm()
+    this.authForm = this.createLoginForm()
     effect(() => {
       if (this.formType()) {
-
+        this.authForm = this.formType() === "login" ? this.createLoginForm() : this.createSignUpForm()
         this.authFormControls.set(Object.keys(this.authForm.controls))
       }
     });
@@ -78,11 +79,11 @@ export class Auth {
     return new FormGroup({
         name: new FormControl("", [Validators.required, this.minLengthValidator]),
         email: new FormControl("", [Validators.required, Validators.email]),
+        telephone: new FormControl("", [Validators.required, ghPhoneValidator()]),
         password: new FormControl("", [Validators.required, strongPasswordValidator()]),
         confirm_password: new FormControl("", [Validators.required, strongPasswordValidator()]),
         region: new FormControl("", [Validators.required, this.minLengthValidator]),
         city: new FormControl("", [Validators.required, this.minLengthValidator]),
-        telephone: new FormControl("", [Validators.required, ghPhoneValidator()]),
       },
       {
         validators: [matchPasswordValidator('password', 'confirmPassword')]
