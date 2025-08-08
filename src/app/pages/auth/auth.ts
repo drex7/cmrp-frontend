8164b01761bcad4cf3d8defbfc766cf39f34f75e
Emmanuel Kpendo <devekkx@gmail.com>
2025-08-08 08:17:52 +0000

    feature: create Ghana phone number validator

--- src/app/pages/auth/auth.ts
+++ src/app/pages/auth/auth.ts
@@ -1,18 +1,82 @@
-import {Component} from '@angular/core';
+import {Component, effect, signal} from '@angular/core';
 import {Card} from 'primeng/card';
 import {FloatLabel} from 'primeng/floatlabel';
 import {InputText} from 'primeng/inputtext';
+import {FormControl, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
+import {cn, ghPhoneValidator, strongPasswordValidator} from '@/lib/utils';
+import {TitleCasePipe} from '@angular/common';
+import {ButtonDirective} from 'primeng/button';
+import {Password} from 'primeng/password';
+import {InputMask} from 'primeng/inputmask';
 
 @Component({
   selector: 'cmrp-auth',
   imports: [
     Card,
     FloatLabel,
-    InputText
+    InputText,
+    ReactiveFormsModule,
+    TitleCasePipe,
+    ButtonDirective,
+    Password,
+    InputMask
   ],
   templateUrl: './auth.html',
   styleUrl: './auth.css'
 })
 export class Auth {
+  protected authForm: FormGroup;
+  protected authFormControls = signal<string[]>([]);
+  protected formType = signal<"login" | "signup">("signup")
+  protected minLengthValidator = Validators.minLength(5);
+  protected readonly cn = cn;
 
+
+  constructor() {
+    this.authForm = this.formType() === "login" ? this.createLoginForm() : this.createSignUpForm()
+    effect(() => {
+      if (this.formType()) {
+
+        this.authFormControls.set(Object.keys(this.authForm.controls))
+      }
+    });
+  }
+
+  protected getInputType(key: string): string {
+    const types: { [key: string]: string } = {
+      email: 'email',
+      password: 'password',
+      telephone: 'tel',
+      name: 'text',
+      region: 'text',
+      city: 'text'
+    };
+    return types[key] || 'text';
+  }
+
+  protected onSubmit() {
+    if (this.authForm.valid) {
+      console.log("Form values: ", this.authForm)
+    } else {
+      console.error("Form is not valid")
+    }
+  }
+
+  private createLoginForm() {
+    return new FormGroup({
+      email: new FormControl("", [Validators.required, Validators.email]),
+      password: new FormControl("", [Validators.required]),
+    })
+  }
+
+  private createSignUpForm() {
+    return new FormGroup({
+      name: new FormControl("", [Validators.required, this.minLengthValidator]),
+      email: new FormControl("", [Validators.required, Validators.email]),
+      password: new FormControl("", [Validators.required, strongPasswordValidator()]),
+      region: new FormControl("", [Validators.required, this.minLengthValidator]),
+      city: new FormControl("", [Validators.required, this.minLengthValidator]),
+      telephone: new FormControl("", [Validators.required, ghPhoneValidator()]),
+    })
+  }
 }
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,49 @@
 import {type ClassValue, clsx} from "clsx"
 import {twMerge} from "tailwind-merge"
+import {AbstractControl, ValidationErrors, ValidatorFn} from '@angular/forms';
 
-export function cn(...inputs: ClassValue[]) {
+export const cn = (...inputs: ClassValue[]) => {
   return twMerge(clsx(inputs))
+}
+
+export const strongPasswordValidator = (): ValidatorFn => {
+  return (control: AbstractControl): ValidationErrors | null => {
+    const value = control.value;
+
+    if (!value) return null;
+
+    const hasUpperCase = /[A-Z]/.test(value);
+    const hasLowerCase = /[a-z]/.test(value);
+    const hasNumber = /\d/.test(value);
+    const hasSpecialChar = /[!@#$%^&*(),.?":{}|<>]/.test(value);
+    const hasMinLength = value.length >= 8;
+
+    const passwordValid =
+      hasUpperCase && hasLowerCase && hasNumber && hasSpecialChar && hasMinLength;
+
+    return passwordValid ? null : {
+      strongPassword: {
+        hasUpperCase,
+        hasLowerCase,
+        hasNumber,
+        hasSpecialChar,
+        hasMinLength
+      }
+    };
+  };
+}
+
+
+export const ghPhoneValidator = (): ValidatorFn => {
+  return (control: AbstractControl): ValidationErrors | null => {
+    const value: string = control.value;
+
+    const ghPhoneRegex = /^(024|054|055|059|020|050|026|056|027|057)\d{7}$/;
+
+    if (!value) return null;
+
+    return ghPhoneRegex.test(value)
+      ? null
+      : {invalidGhanaPhone: true};
+  };
 }
