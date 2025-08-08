56028a657824a40c5723ecc8e93135bb78f8a721
Emmanuel Kpendo <devekkx@gmail.com>
2025-08-08 15:57:07 +0000

    chore: configure amplify

--- src/app/cmrp.ts
+++ src/app/cmrp.ts
@@ -1,32 +1,38 @@
 import {Component} from '@angular/core';
 import {RouterOutlet} from '@angular/router';
+import {Amplify} from 'aws-amplify';
+import outputs from '../../amplify_outputs.json';
 
 @Component({
-    selector: 'app-root',
-    imports: [RouterOutlet],
-    templateUrl: './cmrp.html',
-    styleUrl: './cmrp.css'
+  selector: 'app-root',
+  imports: [RouterOutlet],
+  templateUrl: './cmrp.html',
+  styleUrl: './cmrp.css'
 })
 export class Cmrp {
-    // protected destroy$ = new Subject<void>();
-    // protected breakpointObserver = inject(BreakpointObserver);
-    // protected isSmallerScreen = signal(false)
-    //
-    // // isSmallScreen = this.breakpointObserver.isMatched('(max-width: 599px)');
-    //
-    // constructor() {
-    //     this.breakpointObserver
-    //         .observe([Breakpoints.Handset, Breakpoints.Tablet])
-    //         .pipe(
-    //             takeUntil(this.destroy$),
-    //             map(res => res.matches),
-    //         ).subscribe(res => {
-    //         this.isSmallerScreen.set(res);
-    //     })
-    // }
-    //
-    // ngOnDestroy() {
-    //     this.destroy$.next()
-    //     this.destroy$.complete()
-    // }
+  constructor() {
+    Amplify.configure(outputs);
+  }
+
+  // protected destroy$ = new Subject<void>();
+  // protected breakpointObserver = inject(BreakpointObserver);
+  // protected isSmallerScreen = signal(false)
+  //
+  // // isSmallScreen = this.breakpointObserver.isMatched('(max-width: 599px)');
+  //
+  // constructor() {
+  //     this.breakpointObserver
+  //         .observe([Breakpoints.Handset, Breakpoints.Tablet])
+  //         .pipe(
+  //             takeUntil(this.destroy$),
+  //             map(res => res.matches),
+  //         ).subscribe(res => {
+  //         this.isSmallerScreen.set(res);
+  //     })
+  // }
+  //
+  // ngOnDestroy() {
+  //     this.destroy$.next()
+  //     this.destroy$.complete()
+  // }
 }
