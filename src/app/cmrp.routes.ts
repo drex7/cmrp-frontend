c6267cfb6c1deecf0bb87c55f2eac4b3e2e91b4f
Emmanuel Kpendo <devekkx@gmail.com>
2025-08-11 08:29:53 +0000

    feature: start dashboard layout

--- src/app/cmrp.routes.ts
+++ src/app/cmrp.routes.ts
@@ -2,7 +2,14 @@ import {Routes} from '@angular/router';
 
 const ReportsPage = () => import("@/pages/reports/reports").then(m => m.Reports);
 const AuthPage = () => import("@/pages/auth/auth").then(m => m.Auth);
+const DashboardPage = () => import("@/pages/dashboard/dashboard").then(m => m.Dashboard);
+
 export const routes: Routes = [
+  {
+    title: "CMRP - Dashboard",
+    path: "dashboard",
+    loadComponent: DashboardPage,
+  },
   {
     title: "CMRP - Reports",
     path: "reports",
@@ -11,5 +18,6 @@ export const routes: Routes = [
   {
     path: "auth",
     loadComponent: AuthPage,
-  }
+  },
+
 ];
