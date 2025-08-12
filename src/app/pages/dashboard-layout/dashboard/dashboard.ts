9d68142e0526bc1592a845bcb0cc4b697528a32a
Emmanuel Kpendo <devekkx@gmail.com>
2025-08-12 00:31:00 +0000

    feature: add dashbaord cards

--- src/app/pages/dashboard-layout/dashboard/dashboard.ts
+++ src/app/pages/dashboard-layout/dashboard/dashboard.ts
@@ -1,17 +1,12 @@
 import {Component} from '@angular/core';
-import {dashboardData} from "@/constants/index";
-import {RouterLink, RouterLinkActive, RouterOutlet} from '@angular/router';
-import {Avatar} from 'primeng/avatar';
-import {ButtonDirective} from 'primeng/button';
+import {dashboardData, incidentsData} from "@/constants/index";
+import {Card} from 'primeng/card';
+import {cn} from '@/lib/utils';
 
 @Component({
   selector: 'cmrp-dashboard',
   imports: [
-    RouterLink,
-    RouterLinkActive,
-    Avatar,
-    ButtonDirective,
-    RouterOutlet
+    Card
   ],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.css'
@@ -19,4 +14,6 @@ import {ButtonDirective} from 'primeng/button';
 export class Dashboard {
 
   protected readonly dashboardData = dashboardData;
+  protected readonly incidentsData = incidentsData;
+  protected readonly cn = cn;
 }
--- src/constants/index.ts
+++ src/constants/index.ts
@@ -15,9 +15,41 @@ export const dashboardData = [
     link: "/dashboard/users",
     icon: PrimeIcons.USERS
   },
+  // {
+  //   title: "Roles and Permissions",
+  //   link: "/dashboard/roles",
+  //   icon: PrimeIcons.SHIELD
+  // }
+] as const
+
+export const incidentsData = [
   {
-    title: "Roles and Permissions",
-    link: "/dashboard/roles",
-    icon: PrimeIcons.SHIELD
-  }
+    title: "Total incidents",
+    description: "All reported incidents",
+    increment: true,
+    incOrDecValue: 12,
+    total: 156,
+    icon: PrimeIcons.EXCLAMATION_TRIANGLE
+  }, {
+    title: "pending review",
+    description: "awaiting assignment",
+    increment: false,
+    incOrDecValue: 8,
+    total: 24,
+    icon: PrimeIcons.CLOCK
+  }, {
+    title: "in progress",
+    description: "currently being resolved",
+    increment: true,
+    incOrDecValue: 15,
+    total: 18,
+    icon: PrimeIcons.CHART_LINE
+  }, {
+    title: "resolved",
+    description: "successfully completed",
+    increment: true,
+    incOrDecValue: 25,
+    total: 144,
+    icon: PrimeIcons.CHECK_CIRCLE
+  },
 ] as const
